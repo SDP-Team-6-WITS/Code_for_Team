dad81e27c16ba202d6f15b768ab33faf1c7a5943
Jony Phiri <2456518@students.wits.ac.za>
2024-09-30 19:37:38 +0200

    add update

--- views/profile.js
+++ views/profile.js
@@ -33,5 +33,42 @@ document.addEventListener('DOMContentLoaded', () => {
             password.value = profile.password;
         }
 
+        const update = document.getElementById('update');
+update.addEventListener('submit', () => {
+    const tutor = localStorage.getItem('userId');
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const qualification = document.getElementById('qualification').value;
+    const courses = document.getElementById('courses').value;
+    const password = document.getElementById('password').value;
+
+    const updatedProfile = {
+        fname: name.split(' ')[0],
+        lname: name.split(' ')[1],
+        email: email,
+        qualifications: qualification,
+        courses: courses,
+        password: password
+    }
+
+    fetch(`http://localhost:3000/api/users/${tutor}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedProfile)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            alert('Profile updated successfully');
+            window.location.href = 'profile.html';
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+})
+
         getTutorProfile(tutor);
 })
+
